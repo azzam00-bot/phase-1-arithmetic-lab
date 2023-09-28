f28dd1d8931fa0eaae65513f00e520d220cb4da1
Azzam ALmasslamany <azzammeri24@gmail.com>
2023-09-28 15:07:14 +0300

    I write the full code here

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+// Task 1
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+// Task 2
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Task 3
+let num3 = 45;
+let num4 = 41;
+let mod = num3 % num4;
+
+// Task 4
+let numbers = [10, 5, 20, 15];
+let max = Math.max(...numbers);
